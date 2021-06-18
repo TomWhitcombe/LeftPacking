23f5321f441236c638d309cbfddd5525ae3ac9c8
tom.whitcombe <tom.whitcombe@rocketmail.com>
2021-06-18 14:42:36 +0100

    Added updated test c file for example usage

--- LeftPackTest.c
+++ LeftPackTest.c
@@ -1,13 +1,27 @@
 #include "SSE2LeftPack.h"
+#include "SOA_AOS.h"
 
 int main(int argc, char const *argv[])
 {
-    float floats [4] = {1.0f,2.0f,3.0f, 4.0f};
-    unsigned int mask[4] = { SELECTION_MASK(1,1,1,1) };
+    //remnants of the left pack test- yes I manually went though all 16 types. Sue me
+    {
+        float floats [4] = {1.0f,2.0f,3.0f, 4.0f};
+        unsigned int mask[4] = { SELECTION_MASK(1,1,1,1) };
 
-    __m128 __floats = _mm_load_ps(floats);
-    __m128 __comps = _mm_load_ps((float*)mask);
+        __m128 __floats = _mm_load_ps(floats);
+        __m128 __comps = _mm_load_ps((float*)mask);
 
-    __m128 packed = _mm_leftPack_ps(__floats, __comps);
+        __m128 packed = _mm_leftPack_ps(__floats, __comps);
+    }
+
+    //Transpose test
+    {
+        __m128 xs = _mm_set_ps(7,5,3,1);
+        __m128 ys = _mm_set_ps(8,6,4,2);
+
+        vec2Out_t transposed = vec2_SOA2AOS(xs, ys);
+
+        vec2Out_t transposedBack = vec2_AOS2SOA(transposed.a, transposed.b);
+    }
     return 0;
 }
